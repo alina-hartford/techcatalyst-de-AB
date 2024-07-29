04402552ba317b9e351afd6bf9af9b97a650be44
alina-hartford <alina.baby@thehartford.com>
2024-07-29 16:04:38 +0000

    streamlit lab

--- activities/week7/main.py
+++ activities/week7/main.py
@@ -4,16 +4,20 @@ import streamlit as st
 import boto3
 
 
-#config = configparser.ConfigParser()
+config = configparser.ConfigParser()
 
-#config.read('aws.cfg')
+config.read('aws.cfg')
 
-#aws_access_key = config['AWS']['aws_access_key_id']
-#aws_secret_key = config['AWS']['aws_secret_access_key']
+aws_access_key = config['AWS']['aws_access_key_id']
+aws_secret_key = config['AWS']['aws_secret_access_key']
 
-#s3 = boto3.client('s3', aws_access_key_id = aws_access_key, aws_secret_access_key = aws_secret_key)
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=aws_access_key,
+    aws_secret_access_key=aws_secret_key
+)
 
-st.title['NYC Taxi Data Analysis']
+st.title('NYC Taxi Data Analysis')
 
 #output = s3.download_file(Bucket="capstone-techcatalyst-raw", Key='yellow_taxi")
 
@@ -21,33 +25,82 @@ df = pd.read_parquet('nyc.parquet')
 
 cols = st.multiselect('Select Columns', df.columns)
 
-if len(cols) > 0:
-    st.dataframe(df[cols])
+#if len(cols) > 0:
+#    st.dataframe(df[cols])
 
-if st.chckbox("Show Data"):
-    st.dataframe(df[cols].head(20))
+#if st.chckbox("Show Data"):
+#    st.dataframe(df[cols].head(20))
 
 #read CSV file
-file = st.file_uploader("Upload your CSV data", type = ["csv"])
-st.write(file)
+#file = st.file_uploader("Upload your CSV data", type = ["csv"])
+#st.write(file)
 
-read_file = pd.read_csv(file)
+#read_file = pd.read_csv(file)
 
-st.dataframe(read_file.head(10))
-
-
-ts = df[['tpep_pickup_datetime', 'total_amount']]
+#st.dataframe(read_file.head(10))
 
 
-if st.chckbox("Show Line Chart"):
-    st.line_chart(ts_sum set_index("date"))
-
-st.write("Top 10 Pick up Locations")
-st.dataframe(df["PULocationID"]).value_counts().head()
-
-st.write('Filter data by date')
-date = st.date_input('Date', df['tpep_pickup_datetime'].max())
-st.write(df[df['tpep_pickup_datetime'].dt.date == date])
+#ts = df[['tpep_pickup_datetime', 'total_amount']]
 
 
-st.balloons()
+#if st.chckbox("Show Line Chart"):
+#    st.line_chart(ts_sum set_index("date"))
+
+#st.write("Top 10 Pick up Locations")
+#st.dataframe(df["PULocationID"]).value_counts().head()
+
+#st.write('Filter data by date')
+#date = st.date_input('Date', df['tpep_pickup_datetime'].max())
+#st.write(df[df['tpep_pickup_datetime'].dt.date == date])
+
+
+#st.balloons()
+
+
+
+
+
+
+
+
+# output = s3.download_file(Bucket="capstone-techcatalyst-raw", Key='yellow_taxi/yellow_tripdata_2023-09.parquet', Filename='nyc.parquet')
+
+
+#cols = st.multiselect('Select Columns', df.columns)
+
+#if st.checkbox("Show Data"):
+#    st.dataframe(df[cols].head(20))
+
+
+    # read CSV file
+#    file = st.file_uploader("Upload your CSV data", type=["csv"])
+ #   st.write(file)
+
+#    ts = df[['tpep_pickup_datetime', 'total_amount']]
+#    ts['date'] = df['tpep_pickup_datetime'].dt.date
+#    ts_sum = ts.groupby('date')[['total_amount']].mean()
+#    ts_sum.reset_index(inplace=True)
+#    ts_sum['date'] = pd.to_datetime(ts_sum['date'])
+
+#    if st.checkbox('Show line chart'):
+ #       st.line_chart(ts_sum.set_index('date'))
+
+
+  #  st.write('Top 10 Pick up Locations')
+  #  st.dataframe(df['PULocationID'].value_counts().head(10))
+
+  #  st.bar_chart(df['PULocationID'].value_counts().head(10))
+
+    # example using selectbox to filter data
+  #  st.write('Filter data by payment type')
+  #  payment_type = st.selectbox('Payment Type', df['payment_type'].unique())
+  #  st.write(df[df['payment_type'] == payment_type])
+
+
+    # add calendar widget to filter data by date
+ #   st.write('Filter data by date')
+
+ #   date = st.date_input('Date', df['tpep_pickup_datetime'].max())
+  #  st.write(df[df['tpep_pickup_datetime'].dt.date == date])
+
+#st.divider()
